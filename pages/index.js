f1d1774dbcf5cd607ee93f7f77a6407d7e232faa
Lunnatica <paulalopezantelo@gmail.com>
2019-12-09 15:37:46 +0000

    Created layout component to style all the pages

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-// This is the Link API
-import Link from 'next/link'
-import Header from '../components/Header'
+import Layout from '../components/MyLayout';
 
-
-const Index = () => (
-  <div>
-    <Header />
-    <p>Hello Next.js</p>
-  </div>
-);
-
-export default Index
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  );
+}
